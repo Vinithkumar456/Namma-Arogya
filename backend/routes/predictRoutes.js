403049d1776ea40685d27762c8510fdc02c09656
Vinithkumar456 <153282482+Vinithkumar456@users.noreply.github.com>
2025-03-29 12:06:48 +0530

    Delete backend directory

--- backend/routes/predictRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { predictDisease } = require("../controllers/predictController");
-
-const router = express.Router();
-
-router.post("/p1", predictDisease);
-
-module.exports = router;
